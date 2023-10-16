522b55dacdad15a034448891720cc3cf56bab048
jcarlo <jcarlo@outlook.com>
2023-10-16 15:14:00 -0300

    prox. implementar lógica de duelos

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -7,18 +7,26 @@ const state = {
     cardSprites: {
         avatar: document.getElementById("card-image"),
         name: document.getElementById("card-name"),
-        type: document.getElementById("card-type"),
+        type: document.getElementById("card-type")
     },
     fieldCards: {
         player: document.getElementById("player-field-card"),
-        computer: document.getElementById("copmuter-field-card"),
+        computer: document.getElementById("computer-field-card")
     },
-    button: document.getElementById("next-duel"),
+    playerSides: {
+        player1: "player-cards",
+        player1BOX: document.querySelector("#player-cards"),
+        computer: "computer-cards",
+        computerBOX: document.querySelector("#computer-cards")
+    },
+    actions: {
+        button: document.getElementById("next-duel")
+    }
 };
 
 const playerSides = {
-    player1: "player-field-card",
-    computer: "computer-field-card"
+    player1: "player-cards",
+    computer: "computer-cards"
 }
 
 const pathImages = "./src/assets/icons/";
@@ -53,6 +61,63 @@ const cardData = [
 function init() {
     drawCards(5, playerSides.player1);
     drawCards(5, playerSides.computer);
+}
+
+async function getRandomCardId() {
+    const randomIndex = Math.floor(Math.random() * cardData.length);
+    return cardData[randomIndex].id;
+}
+
+async function createCardImage(IdCard, fieldSide) {
+    const cardImage = document.createElement("img");
+    cardImage.setAttribute("height", "100px");
+    cardImage.setAttribute("src", "./src/assets/icons/card-back.png");
+    cardImage.setAttribute("data-id", IdCard);
+    cardImage.classList.add("card");
+
+    if(fieldSide === playerSides.player1) {
+        cardImage.addEventListener("mouseover", () => {
+            drawSelectCard(IdCard);
+        });
+        
+        cardImage.addEventListener("click", () => {
+            setCardsField(cardImage.getAttribute("data-id"));
+        });
+    }
+
+    return cardImage;
+}
+
+async function setCardsField(cardId) {
+    await removeAllCardsImages();
+
+    let computerCardId = await getRandomCardId();
+
+    state.fieldCards.player.style.display = "block";
+    state.fieldCards.computer.style.display = "block";
+
+    state.fieldCards.player.src = cardData[cardId].img;
+    state.fieldCards.computer.src = cardData[computerCardId].img;
+
+    let duelResults = await checkDuelResults(cardId, computerCardId);
+
+    await updateScore();
+    await drawButton(duelResults);
+}
+
+async function removeAllCardsImages() {
+    let { computerBOX, player1BOX } = state.playerSides;
+    let imgElements = computerBOX.querySelectorAll("img");
+    imgElements.forEach((img) => img.remove());
+    
+    imgElements = player1BOX.querySelectorAll("img");
+    imgElements.forEach((img) => img.remove());
+}
+
+async function drawSelectCard(index) {
+    state.cardSprites.avatar.src = cardData[index].img;
+    state.cardSprites.name.innerText = cardData[index].name;
+    state.cardSprites.type.innerText = "Attribute : " + cardData[index].type;
 }
 
 async function drawCards(cardNumbers, fieldSide) {
